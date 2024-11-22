2ab95ba290a49c918a7d6a5f215761afa572caf0
SatvikDronavalli <send2satvikd@gmail.com>
2024-11-22 11:35:38 -0500

    Update data.py
    I added the data cleaning code for the IMU dataset.

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
 import pandas
+demographics = pandas.read_csv("01_demography_sppb_mmse.xlsx")
+gait_params = pandas.read_csv("05_gait_parameters.xlsx")
+demographics.rename(columns={"self_reported_falls": "at risk of falls"}, inplace=True)
+demographics = demographics[demographics["at risk fo falls"] != 0].iloc[20:]
+valid_IDs = demographics["subject_id"].tolist()
+demographics = demographics[demographics["subject_id"].isin(valid_IDs)]
+y = gait_params["at risk of falls"]
+
