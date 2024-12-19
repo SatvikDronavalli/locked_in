55797eff783628171e7538bdc26bd68e2b18c164
SatvikDronavalli <send2satvikd@gmail.com>
2024-12-19 12:43:20 -0500

    Redid code
    I changed the file reading system and path creation, so the code should work as intended. I also downloaded necessary packages into vscode.

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
-import pandas
-demographics = pandas.read_csv("01_demography_sppb_mmse.xlsx")
-gait_params = pandas.read_csv("05_gait_parameters.xlsx")
+import pandas as pd
+from pathlib import Path
+gait_file = Path("IMU_Data") / "05_gait_parameters.xlsx"
+demographics_file = Path ("IMU_Data") / "01_demography_sppb_mmse.xlsx"
+gait_params = pd.read_excel(gait_file)
+demographics = pd.read_excel(demographics_file)
 demographics.rename(columns={"self_reported_falls": "at risk of falls"}, inplace=True)
 demographics = demographics[demographics["at risk fo falls"] != 0].iloc[20:]
 valid_IDs = demographics["subject_id"].tolist()
 demographics = demographics[demographics["subject_id"].isin(valid_IDs)]
 y = gait_params["at risk of falls"]
-
