7ff27dd78bb6f912dd0d7eb95b08561f2758132d
Satvik <send2satvikd@gmail.com>
2024-12-25 12:38:45 -0500

    I added SMOTE sampling to the accelerometer data

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 # Setup
 import pandas as pd
 from pathlib import Path
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
 pd.set_option('display.max_colwidth',None)
 pd.set_option('display.max_rows',None)
 pd.set_option('display.width',None)
@@ -28,14 +30,21 @@ for i in range(0, len(gait_params)):
     else:
         gait_params.loc[i,"Name"] = curr_name
 dem_index = 0 
-# Marking patients in the gait_parameters dataset as at risk or not at risk of falls
+# Marking non-excess patients in the gait_parameters dataset as at risk or not at risk of falls
 gait_params["at risk of falls"] = [None] * len(gait_params)
 for i in range(0, len(gait_params)):
     if gait_params.loc[i,"Name"] != demographics.loc[dem_index,"Subject ID"]:
         dem_index += 1
     gait_params.loc[i,"at risk of falls"] = demographics.loc[dem_index,"at risk of falls"]
+# Removing excess negative values
 for i in range(0, len(gait_params)):
     if gait_params.loc[i,"at risk of falls"] not in range(0,2):
         gait_params.drop(i,inplace=True)
-# Dropping irrelevant columns
+print(f"Final Length = {len(gait_params)}")
+# Dropping irrelevant columns and balancing dataset with SMOTE
 gait_params.drop(columns=["Left_Limp_Index", "Right_Limp_Index", "Left_Foot_Off", "Right_Foot_Off", "Gait Duration after data crop"],inplace=True)
+X = gait_params.drop(columns=["at risk of falls", "Name"]) # Independent Variable
+y = gait_params["at risk of falls"].astype(int) # Dependent Variable
+smote = SMOTE(sampling_strategy={1:100},random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
