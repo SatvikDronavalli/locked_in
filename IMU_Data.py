e8d98cdbfd0ef26e9d7ae2634be70181e29d2875
SatvikDronavalli <send2satvikd@gmail.com>
2025-02-02 15:43:46 -0500

    Update IMU_Data.py
    turned output dataframes into np arrays for compatibility with force data

--- IMU_Data.py
+++ IMU_Data.py
@@ -40,7 +40,7 @@ for i in range(0, len(gait_params)):
 for i in range(0, len(gait_params)):
     if gait_params.loc[i,"at risk of falls"] not in range(0,2):
         gait_params.drop(i,inplace=True)
-print(f"Final Length = {len(gait_params)}")
+#print(f"Final Length = {len(gait_params)}")
 # Dropping irrelevant columns and balancing dataset with SMOTE
 gait_params.drop(columns=["Left_Limp_Index", "Right_Limp_Index", "Left_Foot_Off", "Right_Foot_Off", "Gait Duration after data crop"],inplace=True)
 X = gait_params.drop(columns=["at risk of falls", "Name"]) # Independent Variable
@@ -48,3 +48,4 @@ y = gait_params["at risk of falls"].astype(int) # Dependent Variable
 smote = SMOTE(sampling_strategy={1:100},random_state=42)
 X_resampled, y_resampled = smote.fit_resample(X,y)
 X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = X_train.to_numpy(), X_test.to_numpy(), y_train.to_numpy(), y_test.to_numpy()
