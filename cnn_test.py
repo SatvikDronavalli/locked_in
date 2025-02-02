bab8635cd94dfc6c1c8c57bcc11b1c4773c5a127
SatvikDronavalli <send2satvikd@gmail.com>
2025-02-02 16:56:38 -0500

    Create cnn_test.py
    assessment of cnn model

---
+++ cnn_test.py
@@ -0,0 +1,9 @@
+from tensorflow.keras.models import load_model
+from Force_Data import gait_x_test, gait_y_test
+# Load the best model after training
+best_model = load_model('best_fall_risk_model.keras')
+
+# Evaluate the best model on the testing data
+test_loss, test_accuracy = best_model.evaluate(gait_x_test, gait_y_test)  # Pass y_test here
+print(f'Test Loss: {test_loss}')
+print(f'Test Accuracy: {test_accuracy}')
