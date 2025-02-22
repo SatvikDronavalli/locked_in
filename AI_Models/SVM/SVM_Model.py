769fc86a70675f858ed8e4657debb4a8dfa6f074
SatvikDronavalli <send2satvikd@gmail.com>
2025-02-22 13:48:20 -0500

    Update SVM_Model.py

--- AI_Models/SVM/SVM_Model.py
+++ AI_Models/SVM/SVM_Model.py
@@ -1,14 +1,17 @@
 import numpy as np
 import tensorflow as tf
 import seaborn as sns
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.inspection import permutation_importance
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
 
 # Make predictions
 from sklearn.model_selection import GridSearchCV
-from Force_Data import gait_x_train, gait_y_train, gait_x_test, gait_y_test
-from IMU_Data import X_train, X_test, y_train, y_test
+from IMU_Data import X_train, X_test, y_train, y_test, params
+print(len(X_train),len(X_test),len(y_train),len(y_test))
 #sns.pairplot(X_train,hue='at risk of falls')
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
@@ -39,6 +42,26 @@ precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1-score: {f1:.4f}")
+
 # Full classification report
 # meta learning
-print("\nClassification Report:\n", classification_report(y_test, y_pred))
+'''
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='d')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
+print("\nClassification Report:\n", classification_report(y_test, y_pred))'''
+
+
+#TODO: Determine weights that are most important
+
+perm_importance = permutation_importance(svm_model, X_test, y_test)
+sorted_idx = perm_importance.importances_mean.argsort()
+plt.barh(params[sorted_idx], perm_importance.importances_mean[sorted_idx])
+plt.xlabel("Permutation Importance")
+plt.show()
