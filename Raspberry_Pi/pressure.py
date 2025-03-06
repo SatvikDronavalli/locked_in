b855b8436bc4b963d5a316a24cbc4366af597930
SatvikDronavalli <send2satvikd@gmail.com>
2025-03-06 12:37:09 -0500

    added initialization

--- Raspberry_Pi/pressure.py
+++ Raspberry_Pi/pressure.py
@@ -4,6 +4,35 @@ import board
 import busio
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = bmp581.BMP581(i2c,0x47)
+started = False
+idx = 1
+readings = 0
+starting_pressure = 0
+print("get ready for initializing 0")
+time.sleep(5)
+print("init started")
+time.sleep(1)
+#==========foot in air=================
+xmin = None
+sum1 = 0
+for i in range(0, 10):
+	sum1 += sensor.pressure
+	time.sleep(0.5)
+xmin = round(sum1/10,5)
+print("get ready for initializing 1")
+time.sleep(5)
+print("init started")
+time.sleep(1)
+#==========foot on ground==============
+xmax = None
+sum2 = 0
+for i in range(0, 10):
+	sum2 += sensor.pressure
+	time.sleep(0.5)
+xmax = round(sum2/10,5)
+print(f"min force is {xmin} and max force is {xmax}")
+n = lambda x: round((x-xmin)/(xmax-xmin),5)
 while True:
-    print(sensor.pressure)66
-    time.sleep(0.5)
+	x = sensor.pressure
+	print(n(x))
+	time.sleep(0.5)
