37ac66ea77e616f339a270821b0e64c6771e56fd
SatvikDronavalli <send2satvikd@gmail.com>
2025-03-12 19:53:32 -0400

    Update IMU_data.py
    added info for pi svm

--- IMU_Data/IMU_data.py
+++ IMU_Data/IMU_data.py
@@ -36,17 +36,40 @@ for i in range(0, len(gait_params)):
     if gait_params.loc[i,"Name"] != demographics.loc[dem_index,"Subject ID"]:
         dem_index += 1
     gait_params.loc[i,"at risk of falls"] = demographics.loc[dem_index,"at risk of falls"]
-# Removing excess negative values
+# Removing excess negative values (ones with blank values)
 for i in range(0, len(gait_params)):
     if gait_params.loc[i,"at risk of falls"] not in range(0,2):
         gait_params.drop(i,inplace=True)
 #print(f"Final Length = {len(gait_params)}")
 # Dropping irrelevant columns and balancing dataset with SMOTE
-gait_params.drop(columns=["Left_Limp_Index", "Right_Limp_Index", "Left_Foot_Off", "Right_Foot_Off", "Gait Duration after data crop"],inplace=True)
+new = gait_params.copy()
+new = gait_params[["Left_Cadence ","Right_Cadence ","Left_Stride_Length","Right_Stride_Length","at risk of falls","Name"]]
+gait_params.drop(columns=["Walk","Left_Limp_Index", "Right_Limp_Index", "Left_Foot_Off", "Right_Foot_Off", "Gait Duration after data crop"],inplace=True)
 X = gait_params.drop(columns=["at risk of falls", "Name"]) # Independent Variable
-params = X.columns.to_numpy()
 y = gait_params["at risk of falls"].astype(int) # Dependent Variable
+one_cnt = 0
+zero_cnt = 0
+for i in y.to_numpy():
+    if i == 0:
+        zero_cnt += 1
+    else:
+        one_cnt += 1
+params = X.columns.to_numpy()
 smote = SMOTE(sampling_strategy={1:100},random_state=42)
 X_resampled, y_resampled = smote.fit_resample(X,y)
+X_resampled.to_csv("testing.csv")
 X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
-X_train, X_test, y_train, y_test = X_train.to_numpy(), X_test.to_numpy(), y_train.to_numpy(), y_test.to_numpy()
+# ============Processing for new model=============
+new_X = new.drop(columns=["at risk of falls", "Name"]) # Independent Variable
+new_y = new["at risk of falls"].astype(int) # Dependent Variable
+one_cnt = 0
+zero_cnt = 0
+for i in new_y.to_numpy():
+    if i == 0:
+        zero_cnt += 1
+    else:
+        one_cnt += 1
+params = new_X.columns.to_numpy()
+smote = SMOTE(sampling_strategy={1:100},random_state=42)
+X_resampled, y_resampled = smote.fit_resample(new_X,new_y)
+new_X_train, new_X_test, new_y_train, new_y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
